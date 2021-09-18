152dd3e8e83cc4187c7ee52b1b566266edf613e8
Korzahk <ecc16001@byui.edu>
2021-09-17 20:45:59 -0700

    dynamic table of contents v8

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
+let data = ["Ram", "Shyam", "Sita", "Gita"];
 
-const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
-    }
-]
+    let list = document.getElementById("myList");
 
-document.getElementById("myList").innerHTML = 5 + 6;
-/* let list = document.getElementById("myList");
-
-let data = ['Ram', 'Shyam', 'Sita', 'Gita' ];
-    
-data.forEach((item)=>{
-  let li = document.createElement("li");
-  li.innerText = item;
-  list.appendChild(li);
-})
+    data.forEach((item) => {
+      let li = document.createElement("li");
+      li.innerText = item;
+      list.appendChild(li);
+    });

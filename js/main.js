897983c9622bfd2a0fbd3e45559e416415b5a999
Korzahk <ecc16001@byui.edu>
2021-09-18 09:12:06 -0700

    commit message

--- js/main.js
+++ js/main.js
@@ -10,6 +10,6 @@ let list = document.getElementById("myList");
 links.forEach((item) => {
   let li = document.createElement("li");
   li.innerText = item.label;
-  li.innerText.url = item.url;
+  li.innerHTML = "...ooo".link("http://www.msn.com");
   list.appendChild(li);
 });

b82d6264b4ec35ee97a46499c0980fefecc919e4
Korzahk <ecc16001@byui.edu>
2021-10-22 12:29:32 -0700

    todoList

--- week06/To-Do.js
+++ week06/To-Do.js
@@ -1,4 +1,15 @@
 function add() {
     let newTodo = document.getElementById("add").value;
     console.log(newTodo);
+    let todoList = document.querySelector("div.todoList")
+    // while (todoList.firstChild) todoList.removeChild(todoList.firstChild);
+    let myTable = document.createElement('table');
+    myTable.className = 'todoListTable';
+    let myTableRow = document.createElement('tr');
+    myTableRow.className = 'todoListTableRow';
+    let myTableData = document.createElement('td');
+    myTableData.innerText = newTodo;
+    myTableRow.append(myTableData);
+    myTable.append(myTableRow);
+    todoList.append(myTable);
 }

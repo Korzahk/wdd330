4c6021c23235795807e0d59cd62b8405c781d4b5
Korzahk <ecc16001@byui.edu>
2021-10-21 17:09:24 -0700

    todo

--- week06/To-Do.js
+++ week06/To-Do.js
@@ -1,6 +1,4 @@
 function add() {
-    console.log("add()");
-}
-function myFunction() {
-    document.getElementById("add").value = "Hello World";
+    let newTodo = document.getElementById("add").value;
+    console.log(newTodo);
 }

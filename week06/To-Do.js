7c678a9275e99cfa26f843956e8ccf1e06f9d078
Korzahk <ecc16001@byui.edu>
2021-10-22 14:14:08 -0700

    todoList

--- week06/To-Do.js
+++ week06/To-Do.js
@@ -1,15 +1,26 @@
 function add() {
     let newTodo = document.getElementById("add").value;
-    console.log(newTodo);
     let todoList = document.querySelector("div.todoList")
     // while (todoList.firstChild) todoList.removeChild(todoList.firstChild);
     let myTable = document.createElement('table');
     myTable.className = 'todoListTable';
     let myTableRow = document.createElement('tr');
-    myTableRow.className = 'todoListTableRow';
+    myTableRow.id = newTodo;
     let myTableData = document.createElement('td');
+    let myCheckbox = document.createElement('input');
+    myCheckbox.type = 'checkbox';
     myTableData.innerText = newTodo;
-    myTableRow.append(myTableData);
+    let deleteButton = document.createElement('input');
+    deleteButton.type = 'button';
+    deleteButton.value = 'x';
+    deleteButton.onclick = function() { deleteTodo(myTable, newTodo); };
+
+    myTableRow.append(myCheckbox, myTableData, deleteButton);
     myTable.append(myTableRow);
     todoList.append(myTable);
+}
+function deleteTodo(myTable, todo) {
+    console.log(todo);
+    todoToDelete = document.getElementById(todo);
+    myTable.removeChild(todoToDelete);
 }
